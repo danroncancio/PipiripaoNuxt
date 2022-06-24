3801aac4d2a028f165898995abb4662456db77a3
Daniel Roncancio <daniel@roncancio.co>
2022-06-24 09:10:33 -0500

    Implementation of 3D view 👓

--- components/Scene.vue
+++ components/Scene.vue
@@ -5,6 +5,37 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+const route = useRoute();
+let activeSize = "4";
+let activeColor = "";
+let activeKidUrl = "";
+let activeSKU = route.query.sku;
+
+const state = reactive({
+  didInter: false,
+});
+
+// TODO: This is retarded.
+const modelUrls = {
+  // 4
+  "/models/4/Modelo.glb": [
+    "/models/4/CT7527/Blanco.glb",
+    "/models/4/CT7527/Negro.glb",
+    "/models/4/OF0426/Blanco.glb",
+    "/models/4/OF0426/Negro.glb",
+  ],
+  // 6
+  "/models/6/Modelo.glb": [
+    "/models/6/PC0426/Rojo.glb",
+    "/models/6/PC0426/Negro.glb",
+  ],
+  // 8
+  "/models/8/Modelo.glb": [
+    "/models/8/PC0426/Rojo.glb",
+    "/models/8/PC0426/Gris.glb",
+  ],
+};
+
 const canvas = ref(null);
 
 const scene = new THREE.Scene();
@@ -18,6 +49,9 @@ camera.position.set(1, 1, 1);
 
 let renderer;
 let controls;
+const raycaster = new THREE.Raycaster();
+raycaster.layers.set(0);
+const pointer = new THREE.Vector2(1, 1);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
@@ -31,6 +65,7 @@ directionalLight.shadow.camera.top = 7;
 directionalLight.shadow.camera.right = 7;
 directionalLight.shadow.camera.bottom = -7;
 directionalLight.position.set(5, 5, 5);
+directionalLight.layers.set(0);
 scene.add(directionalLight);
 
 const floor = new THREE.Mesh(
@@ -42,59 +77,99 @@ floor.rotation.x = MathUtils.degToRad(-90);
 scene.add(floor);
 
 let urls = [];
-urls.push("../assets/models/4/PA0123/Azul/PA0123-Azul.gltf");
-urls.push("../assets/models/4/PA0123/Negro/Negro1.gltf");
 
-let models = {};
+let models = {}; // array for the loadModels function
 let activeGarmentID;
+let activeGarmentIndex;
+
+let kidModel;
+
+let clothesGroup = new THREE.Group();
 
 const loader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath("../assets/models/draco/gltf/");
 loader.setDRACOLoader(dracoLoader);
 
-// Load kid model
-let kidUrl = "../assets/models/4/Modelo.gltf";
-loadModel(kidUrl).then((res) => {
-  res.scene.traverse((node) => {
-    if (node.isMesh) {
-      node.castShadow = true;
-      node.receiveShadow = true;
+function getModelsUrl(size) {
+  activeSize = size;
+  for (let key in modelUrls) {
+    if (key.includes(size)) {
+      activeKidUrl = key;
+      for (let j = 0; j < modelUrls[key].length; j++) {
+        if (modelUrls[key][j].includes(activeSKU)) {
+          urls.push(modelUrls[key][j]);
+        }
+      }
     }
-  });
-  scene.add(res.scene);
-});
+  }
+  console.log(activeSKU, " ", activeSize, " ", activeKidUrl);
+  console.log(urls);
+  loadKid(activeKidUrl);
+  loadGarments(urls);
+}
 
-for (let i = 0; i < urls.length; i++) {
-  loadModel(urls[i]).then((res) => {
+// Load kid model
+function loadKid(url) {
+  let kidUrl = url;
+  loadModel(kidUrl).then((res) => {
     res.scene.traverse((node) => {
+      node.layers.set(0);
       if (node.isMesh) {
         node.castShadow = true;
         node.receiveShadow = true;
       }
     });
-    if (i === 0) {
-      activeGarmentID = res.scene.uuid;
-      scene.add(res.scene);
-    }
-    //scene.add(res.scene)
+    kidModel = res.scene;
+    console.log(kidModel.position);
+    scene.add(kidModel);
   });
-  console.log(urls[i]);
+}
+
+function loadGarments() {
+  console.log("length: ", urls.length);
+  for (let i = 0; i < urls.length; i++) {
+    loadModel(urls[i]).then((res) => {
+      //clothesGroup.add(res.scene);
+      //console.log("resp obj: ", res);
+      res.scene.traverse((node) => {
+        node.layers.set(0);
+        if (node.isMesh) {
+          node.castShadow = true;
+          node.receiveShadow = true;
+        }
+      });
+      if (i === 0) {
+        activeGarmentID = res.scene.uuid;
+        scene.add(res.scene);
+        activeGarmentIndex = 0;
+      }
+      //console.log("Grupo: ", clothesGroup);
+    });
+    //console.log(urls[i]);
+  }
 }
 
 function loadModel(url) {
   if (models[url]) {
     return models[url];
   }
-
   return (models[url] = loader.loadAsync(url, (e) => {}));
 }
 
 function changeGarment() {
-  loadModel("../assets/models/4/PA0123/Negro/Negro1.gltf").then((res) => {
-    let prevGarmentObj = scene.getObjectByProperty("uuid", activeGarmentID);
-    scene.remove();
+  let prevGarmentObj = scene.getObjectByProperty("uuid", activeGarmentID);
+  activeGarmentIndex += 1;
+  if (urls[activeGarmentIndex] === undefined) {
+    activeGarmentIndex = 0;
+  }
+
+  loadModel(urls[activeGarmentIndex]).then((res) => {
+    console.log(res);
+    scene.remove(prevGarmentObj);
     scene.add(res.scene);
+    activeGarmentID = res.scene.uuid;
+    //state.didInter = false;
   });
 }
 
@@ -102,10 +177,34 @@ function showScene() {
   console.log(scene);
 }
 
+function showGroups() {
+  console.log(clothesGroup);
+}
+
+function onPointerMove(event) {
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  //console.log(pointer);
+}
+
+function onPointerClick(event) {
+  //console.log(scene);
+  if (activeGarmentID !== undefined) {
+    const object3D = scene.getObjectByProperty("uuid", activeGarmentID);
+    const intersects = raycaster.intersectObject(object3D, true);
+    if (intersects.length > 0) {
+      changeGarment();
+    }
+  }
+}
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
+  raycaster.setFromCamera(pointer, camera);
 }
 
 function createScene(el) {
@@ -114,6 +213,8 @@ function createScene(el) {
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   controls = new OrbitControls(camera, renderer.domElement);
   controls.autoRotate = false;
+  controls.target = new THREE.Vector3(0, 0.65, 0);
+
   resize();
   animate();
 }
@@ -129,10 +230,17 @@ onMounted(() => {
 });
 
 window.addEventListener("resize", resize);
+window.addEventListener("click", onPointerClick);
+window.addEventListener("pointermove", onPointerMove);
 </script>
 
 <template>
   <button @click="changeGarment">Click Me!</button>
   <button @click="showScene">Scene Me!</button>
+  <button @click="showGroups">Groups Me!</button>
+  <div></div>
+  <button @click="getModelsUrl('4')">4</button>
+  <button @click="getModelsUrl('6')" class="ml-2">6</button>
+  <button @click="getModelsUrl('8')" class="ml-2">8</button>
   <canvas ref="canvas"></canvas>
 </template>
---
+++ pages/3D/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute();
+console.log("3D");
+console.log(route.query.sku);
+</script>
+<template>
+  <ClientOnly>
+    <Scene />
+  </ClientOnly>
+  <Footer />
+</template>
